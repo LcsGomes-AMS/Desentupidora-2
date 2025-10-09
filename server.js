5dcbe4090a9d04fce58a749bbc327cdb218863a0
LcsGomes-AMS <lucashenriquediasgomes1712@gmail.com>
2025-10-09 00:38:53 -0300

    server

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // server.js
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
 const bodyParser = require("body-parser");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
+const sqlite3 = require('sqlite3');
+
 
 
 const app = express();
