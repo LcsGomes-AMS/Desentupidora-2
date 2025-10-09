d5994b22e81eda13b5dc56f0165c1f869723126a
LcsGomes-AMS <lucashenriquediasgomes1712@gmail.com>
2025-10-09 01:18:25 -0300

    atual

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-// server.js
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const PORT = process.env.PORT || 3000;
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
+
+const PORT = 3000
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 const USERS = [
     { username: 'admin', password: '123456' },
 ];
 
-
-
-// Middlewares
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public"))); // serve arquivos da pasta public
-
-// Rota raiz - serve index.html
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = USERS.find(u => u.username === username && u.password === password);
+    if (user){
+        res.json({ sucess: true, message: 'Login realizado com sucesso'});
+    } else {
+        res.status(401).json({ sucess: false, message: 'Usuário ou senha inválidos'});
+    }
 });
 
-// Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+})
